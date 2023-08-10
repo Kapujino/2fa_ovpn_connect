9dd48ab5257d9429bdfdeb2f5c297fbd2205690e
Kapujino <Kapujino@users.noreply.github.com>
2023-08-10 11:14:50 +0200

    cleanup

--- openvpn_2fa.sh
+++ openvpn_2fa.sh
@@ -5,11 +5,11 @@ if [ -z `nmcli -g GENERAL.STATE con show $VPN_CON_NAME` ]; then
     SECRET="2FA_SECRET"
     AUTH_CODE=`oathtool --totp=sha1 -b $SECRET`
     PW=$FIX_PW$AUTH_CODE
-    notify-send 'Connecting Infront VPN' 'NetworkManager should establish the VPN connection' --icon=dialog-information
+    notify-send 'Connecting VPN' 'NetworkManager should establish the VPN connection' --icon=dialog-information
     echo $PW | /usr/bin/nmcli c up $VPN_CON_NAME --ask
 elif [ `nmcli -g GENERAL.STATE con show $VPN_CON_NAME` == "activated" ]; then
     /usr/bin/nmcli c down $VPN_CON_NAME
-    notify-send 'Disconnecting Infront VPN' 'NetworkManager should disconnect the VPN connection' --icon=dialog-information
+    notify-send 'Disconnecting VPN' 'NetworkManager should disconnect the VPN connection' --icon=dialog-information
 else
     echo "no or unknown argument"
 fi
